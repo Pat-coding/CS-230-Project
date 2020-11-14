0136edef2e23c17dc5c006c83db7059841b3beec
Pat <loquetpat@gmail.com>
2020-11-14 19:39:36 +0000

    adjusted a few methods on board.

--- Labyrinth/src/Board.java
+++ Labyrinth/src/Board.java
@@ -2,22 +2,12 @@
  * @author Pat, Ash
  * @version 1.0
  */
-
-
 import Tile;
 import Player;
 
+import java.util.ArrayList;
 
 public class Board {
-    // 90, 180, 270
-
-//    0 1 2 3 4 5
-//  0 A F
-//  1
-//  2
-//  3
-//  4
-//  5
 
     private String nameOfBoard;
     private final int rowSize;
@@ -26,29 +16,34 @@ public class Board {
     private Player[][] playerCoordinates;
 
     //  Tile (Type, Orientation, State, fixed)
-    public Board (String nameOfBoardInput, int[] sizeOfBoard) {
+    public Board(String nameOfBoard, int[] sizeOfBoard) {
         rowSize = sizeOfBoard[0];
         columnSize = sizeOfBoard[1];
-
-        nameOfBoard = nameOfBoardInput;
-        tileCoordinates = new Tile[columnSize][rowSize];
-        playerCoordinates = new Player[columnSize][rowSize];
-
+        this.nameOfBoard = nameOfBoard;
+        tileCoordinates = new Tile[getColumnSize()][getRowSize()];
+        playerCoordinates = new Player[getColumnSize()][getRowSize()];
     }
 
-    public void insertTile (int x, int y, Tile tile) {
+    //new level format
+    public Board(int[] sizeOfBoard, String nameOfBoard) {
+        rowSize = sizeOfBoard[0];
+        columnSize = sizeOfBoard[1];
+        this.nameOfBoard = nameOfBoard;
+    }
+
+    public void insertTile(int x, int y, Tile tile) {
         tileCoordinates[x][y] = tile;
     }
 
-    public void insertPlayer (int x, int y, Player player) {
-        playerCoordinates[x][y] = Player;
+    public void insertPlayer(int x, int y, Player player) {
+        playerCoordinates[x][y] = player;
     }
 
-    public Tile getTileFromBoard (int x, int y) {
+    public Tile getTileFromBoard(int x, int y) {
         return tileCoordinates[x][y];
     }
 
-    public Player getPlayerFromBoard (int x, int y) {
+    public Player getPlayerFromBoard(int x, int y) {
         return playerCoordinates[x][y];
     }
 
@@ -56,25 +51,118 @@ public class Board {
         return rowSize;
     }
 
+    public int getColumnSize() {
+        return columnSize;
+    }
+
     /**
-     * This Method check to see if any Fixed Tiles are present inside of a particular row, of a Board.
+     * This Method check to see if any Fixed Tiles are present inside of a particular row of the board.
      *
      * @param y the row in question
      * @return Boolean result
      */
-
-    public boolean checkTileInsersionRow(int y) {
+    public boolean checkTileInsertionRow(int y) {
         for (int x = 0; x < getRowSize(); x++) {
-            if ((getTileFromBoard(x, y).isFixed || getTileFromBoard (x, y).isFrozen) == True) {
-                return False;
+            if (getTileFromBoard(x, y).isFixed || getTileFromBoard(x, y).isFrozen) {
+                return false;
             }
         }
-        return True;
+        return true;
     }
 
-    //private boolean checkTilePlacementCol(int y) {
-//        for (int i = 0; i < boardY; i++) {
-            // if (tile.fixed == true || tile.frozen == true){return false} something like this, no clue XD
-    //    }
+    /**
+     * This method will check to see if any fixed tiles are present of a particular column of the board.
+     *
+     * @param x the column in question
+     * @return Boolean result
+     */
+    private boolean checkTilePlacementCol(int x) {
+        for (int y = 0; y < getColumnSize(); y++) {
+            if (getPlayerFromBoard(x, y).fixed || getPlayerFromBoard(x, y).frozen) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //need to change, negative index
+    public void setTilesFrozen(int x, int y) {
+        for (int row = x - 1; row < x + 3; row++) {
+            getTileFromBoard(row, y + 1).frozen == true;
+        }
+
+        for (int row = x - 1; row < x + 3; row++) {
+            getTileFromBoard(row, y).frozen == true;
+        }
+
+        for (int row = x - 1; row < x + 3; row++) {
+            getTileFromBoard(row, y - 1).frozen == true;
+        }
+    }
+
+    //need to change, negative index
+    public void setTilesOnFire(int x, int y) {
+        for (int row = x - 1; row < x + 3; row++) {
+            getTileFromBoard(row, y + 1).onFire == true;
+        }
+
+        for (int row = x - 1; row < x + 3; row++) {
+            getTileFromBoard(row, y).onFire == true;
+        }
+
+        for (int row = x - 1; row < x + 3; row++) {
+            getTileFromBoard(row, y - 1).onFire == true;
+        }
+    }
+
+    public enum Cardinals { //add it to the Floor Tile class
+      TOP,
+      BOTTOM,
+      LEFT,
+      RIGHT }
+
+    //for loop, pushing the x or y values back? depending on which cardinal direction
+    public void placeOnNewTile(Cardinals c, int x, int y, Tile tile) { //use enum for access cardinals on tiles
+        if(c.equals("TOP")) {//shift index down from the second last (animations)
+            discardTileToSilkBag(getTileFromBoard(x, getColumnSize()));
+            for(int col = getColumnSize(); col >= 0; col--) {
+                insertTile(x, col + 1, getTileFromBoard(x, col));
+            }
+            insertTile(x, y, tile);
+        }
+
+        if(c.equals("BOTTOM")) {
+            discardTileToSilkBag(getTileFromBoard(x, getColumnSize()));
+            for(int col = 0; col < getColumnSize(); col++) {
+                insertTile(x, col - 1, getTileFromBoard(x, col));
+            }
+            insertTile(x, y, tile);
+        }
+
+        if(c.equals("LEFT")) {
+
+        }
+
+        if(c.equals("RIGHT")) {
+
+        }
+    }
+
+    public void movePlayerFromEndTile() { //case move if end of tile
+
+    }
+
+    public void backTrackPlayer(ArrayList<Integer> tilesVisited, int x, int y) {
+        if(getPlayerFromBoard(x, y).hasBeendoubled) { //should be tracker on last 2 turns
+            insertPlayer(tilesVisited.get(4), tilesVisited.get(5), getPlayerFromBoard(x, y));
+        } else {
+            insertPlayer(tilesVisited.get(2), tilesVisited.get(3), getPlayerFromBoard(x, y));
+        }
+    }
+
+    public void discardTileToSilkBag(Tile tile) {
+        SilkBag.insertTileToBag(tile);
+    }
+
 
 }
