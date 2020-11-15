05e9ca018a36bfec7ecf84c9b08e8a0f4e27bb07
kanep15 <34273244+kanep15@users.noreply.github.com>
2020-11-15 17:43:50 +0000

    added variables tiles visisted,playerInventory,HasBeenBactrakced,PlayerColour,isPlayerTurn and name to player

--- Labyrinth/src/Player.java
+++ Labyrinth/src/Player.java
@@ -1,5 +1,21 @@
+/**
+ * @author Kane
+ * @version 1.0
+ */
+
+import java.util.ArrayList;
+import javafx.scene.paint.Color;
+
 public class Player {
+
     public boolean hasBeenDoubled;
+    private ArrayList<Integer> tilesVisited =  new ArrayList<Integer>();
+    private ArrayList<Tile> playerInventory = new ArrayList<Tile>();
+    private boolean hasBeenBackTracked;
+    private Color playercolor;
+    private boolean isPlayerTurn;
+    private Profile name;
+
 
     public Player(int i, int i1, int i2, String heldPlayerTile, Boolean backTrackCheck) {
     }
