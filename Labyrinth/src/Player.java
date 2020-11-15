dcb69c50e5a8f762d09cd2711a4ec23ef3a794f3
kanep15 <34273244+kanep15@users.noreply.github.com>
2020-11-15 21:46:55 +0000

    added methods; getplayercolor,getplayerInventory,getplayerTurn,incPlayerwin,playerturn

--- Labyrinth/src/Player.java
+++ Labyrinth/src/Player.java
@@ -22,12 +22,33 @@ public class Player {
 
     public Player(Integer integer, int spawnPoint, Object o, Object heldPlayerTile, boolean backTrackCheck) {
     }
+
+    public Color getPlayercolor(){
+        return playercolor;
+    }
+
+    public ArrayList<Tile> getPlayerInventory() {
+        return playerInventory;
+    }
+    public boolean getPlayerTurn(){
+        return  isPlayerTurn;
+    }
+
+
     public int[] getPrevCoordinates(){
         return new int[]{tilesVisited.get(tilesVisited.size()),tilesVisited.get(tilesVisited.size()-1)};
 
     }
     public void getFromSilkBag(Tile pickedTile){
         playerInventory.add(pickedTile);
+    }
+
+    public void incPlayerWin(){
+
+    }
+
+    public void playerTurn(){
+        isPlayerTurn = !isPlayerTurn;
     }
 
 }
