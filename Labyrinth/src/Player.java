124a24a02d9c43e6832fdd4b62a4f368dd3f9f45
kanep15 <34273244+kanep15@users.noreply.github.com>
2020-11-17 14:09:31 +0000

    added javadoc for all methods

--- Labyrinth/src/Player.java
+++ Labyrinth/src/Player.java
@@ -20,23 +20,51 @@ public class Player {
 
     }
 
-
+    /**
+     * This Method returns the players inventory
+     *
+     * @return Tiles held by player
+     */
     public ArrayList<Tile> getPlayerInventory() {
         return playerInventory;
     }
+
+    /**
+     * This Method returns if it is the players turn
+     *
+     * @return boolean result for player turn
+     */
     public boolean getPlayerTurn(){
         return  isPlayerTurn;
     }
 
 
+    /**
+     * This Method returns the players previous coordinates
+     *
+     * @return a array with the players previous x and y coords
+     */
+
     public int[] getPrevCoordinates(){
         return new int[]{tilesVisited.get(tilesVisited.size()),tilesVisited.get(tilesVisited.size()-1)};
 
     }
+
+    /**
+     * This Method adds a tile to the inventory
+     * @param pickedTile
+     *
+     */
     public void getFromSilkBag(Tile pickedTile){
 
         playerInventory.add(pickedTile);
     }
+
+    /**
+     * This Method check if player is at win coords and if so increments that players win stat
+     * @param wincorords
+     *
+     */
 
     public void incPlayerWin(int[] wincorords){
         if (getPrevCoordinates() == wincorords)
@@ -44,10 +72,21 @@ public class Player {
 
 
     }
+    /**
+     * This Method flips player turn after they have finished their turn
+     *
+     * @return Boolean result
+     */
 
     public void playerTurn(){
         isPlayerTurn = !isPlayerTurn;
     }
+
+    /**
+     * This Method returns the chosen tile from the inventory and removes it as its been used
+     * @param index the place of the tile
+     * @return the selected Tile
+     */
 
     public Tile takeFromInventory(int index){
         if(index-1 > playerInventory.size()){
