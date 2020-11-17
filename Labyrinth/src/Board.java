e6eb59552c3963eb61754fd5ee8a07a3f4bf5b8e
Pat <loquetpat@gmail.com>
2020-11-17 13:39:41 +0000

    Added more javadoc for methods. Changed logic for placeOnNewTile. Added an if statement for backTrackPlayer.

--- Labyrinth/src/Board.java
+++ Labyrinth/src/Board.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 /**
+ * The board class structures the game board where the methods are operations that directly affects what is going on
+ * the game board. This includes tiles and players on the game board.
  * @author Pat, Ash
  * @version 1.0
  */
-
-import java.util.ArrayList;
 
 public class Board {
 
@@ -14,6 +15,11 @@ public class Board {
     private Player[][] playerCoordinates;
 
     //  Tile (Type, Orientation, State, fixed)
+    /**
+     * The constructor for a saved level format.
+     * @param nameOfBoard The name of the board.
+     * @param sizeOfBoard The size of the board.
+     */
     public Board(String nameOfBoard, int[] sizeOfBoard) {
         rowSize = sizeOfBoard[0];
         columnSize = sizeOfBoard[1];
@@ -23,49 +29,89 @@ public class Board {
     }
 
     //new level format
-    public Board(int[] sizeOfBoard, String nameOfBoard) {
+    /**
+     * The constructor for a new level format.
+     * @param sizeOfBoard The name of the board.
+     * @param nameOfBoard The size of the board.
+     */
+    private Board(int[] sizeOfBoard, String nameOfBoard) {
         rowSize = sizeOfBoard[0];
         columnSize = sizeOfBoard[1];
         this.setNameOfBoard(nameOfBoard);
     }
 
+    /**
+     * @return The name of the board.
+     */
     private String getNameOfBoard() {
         return nameOfBoard;
     }
 
+    /**
+     * @param nameOfBoard The name of the board.
+     */
     private void setNameOfBoard(String nameOfBoard) {
         this.nameOfBoard = nameOfBoard;
     }
 
+    /**
+     * @param x The x co-ordinate of the tile.
+     * @param y The y co-ordinate of the tile.
+     * @param tile THe tile at the position.
+     */
     public void insertTile(int x, int y, Tile tile) {
         tileCoordinates[x][y] = tile;
     }
 
+    /**
+     *
+     * @param x The x co-ordinate of the player.
+     * @param y The y co-ordinate of the player.
+     * @param player The player at the position.
+     */
     private void insertPlayer(int x, int y, Player player) {
         playerCoordinates[x][y] = player;
     }
 
+    /**
+     * @param x
+     * @param y
+     * @return
+     */
     private Tile getTileFromBoard(int x, int y) {
         return tileCoordinates[x][y];
     }
 
+    /**
+     *
+     * @param x
+     * @param y
+     * @return
+     */
     private Player getPlayerFromBoard(int x, int y) {
         return playerCoordinates[x][y];
     }
 
+    /**
+     *
+     * @return
+     */
     private int getRowSize() {
         return rowSize;
     }
 
+    /**
+     *
+     * @return
+     */
     private int getColumnSize() {
         return columnSize;
     }
 
     /**
      * This Method check to see if any Fixed Tiles are present inside of a particular row of the board.
-     *
-     * @param y the row in question
-     * @return Boolean result
+     * @param y the row in question.
+     * @return Boolean result.
      */
     public boolean checkTileInsertionRow(int y) {
         for (int x = 0; x < getRowSize(); x++) {
@@ -78,9 +124,8 @@ public class Board {
 
     /**
      * This method will check to see if any fixed tiles are present of a particular column of the board.
-     *
-     * @param x the column in question
-     * @return Boolean result
+     * @param x the column in question.
+     * @return Boolean result.
      */
     private boolean checkTilePlacementCol(int x) {
         for (int y = 0; y < getColumnSize(); y++) {
@@ -92,28 +137,34 @@ public class Board {
     }
 
     /**
-     *
-     * @param x
-     * @param y
+     *This method will set tiles to frozen state in a 3x3 area.
+     * @param x The x co-ordinate of the center .
+     * @param y The y co-ordinate of the center.
      */
     public void setTilesFrozen(int x, int y) {
         for (int row = x - 1; row < x + 3; row++) {
-            getTileFromBoard(row, y + 1).frozen = true;
+            if (getTileFromBoard(row, y + 1) != null) {
+                getTileFromBoard(row, y + 1).frozen = true;
+            }
         }
 
         for (int row = x - 1; row < x + 3; row++) {
-            getTileFromBoard(row, y).frozen = true;
+            if (getTileFromBoard(row, y) != null) {
+                getTileFromBoard(row, y).frozen = true;
+            }
         }
 
         for (int row = x - 1; row < x + 3; row++) {
-            getTileFromBoard(row, y - 1).frozen = true;
+            if (getTileFromBoard(row, y - 1) != null) {
+                getTileFromBoard(row, y - 1).frozen = true;
+            }
         }
     }
 
     /**
-     *
-     * @param x
-     * @param y
+     *This method will set tiles on file in a 3x3 area.
+     * @param x The x co-ordinate of the center .
+     * @param y The y co-ordinate of the center.
      */
     public void setTilesOnFire(int x, int y) {
         for (int row = x - 1; row < x + 3; row++) {
@@ -136,18 +187,17 @@ public class Board {
     }
 
     /**
-     * This method will shift tiles depending on their cardinal direction placement
-     *
-     * @param c    The cardinal direction placement
-     * @param x    The x co-ordinate where the player wants to slide tile in
-     * @param y    The y co-ordinate where the player wants to slide tile in
-     * @param tile The tile that is being slided in
+     * This method will shift tiles depending on their cardinal direction placement.
+     * @param c    The cardinal direction placement.
+     * @param x    The x co-ordinate where the player wants to slide tile in.
+     * @param y    The y co-ordinate where the player wants to slide tile in.
+     * @param tile The tile that is being slided in.
      */
     public void placeOnNewTile(Cardinals c, int x, int y, Tile tile) { //use enum for access cardinals on tiles
         if (c == Cardinals.TOP) {//shift index down from the second last (animations)
             discardTileToSilkBag(getTileFromBoard(x, getColumnSize()));
-            for (int col = getColumnSize(); col > 0; col--) {
-                insertTile(x, col, getTileFromBoard(x, col));
+            for (int col = getColumnSize() - 1; col >= 0; col--) {
+                insertTile(x, col, getTileFromBoard(x, col - 1));
             }
             insertTile(x, y, tile);
         }
@@ -155,7 +205,7 @@ public class Board {
         if (c == Cardinals.BOTTOM) {
             discardTileToSilkBag(getTileFromBoard(x, getColumnSize()));
             for (int col = 0; col < getColumnSize(); col++) {
-                insertTile(x, col, getTileFromBoard(x, col));
+                insertTile(x, col, getTileFromBoard(x, col + 1));
             }
             insertTile(x, y, tile);
         }
@@ -163,7 +213,7 @@ public class Board {
         if (c == Cardinals.LEFT) {
             discardTileToSilkBag(getTileFromBoard(getRowSize(), y));
             for (int row = getRowSize(); row > 0; row--) {
-                insertTile(row, getRowSize(), getTileFromBoard(row, y));
+                insertTile(row, getRowSize(), getTileFromBoard(row - 1, y));
             }
             insertTile(x, y, tile);
         }
@@ -171,18 +221,17 @@ public class Board {
         if (c == Cardinals.RIGHT) {
             discardTileToSilkBag(getTileFromBoard(getRowSize(), y));
             for (int row = 0; row < getRowSize(); row++) {
-                insertTile(row, getRowSize(), getTileFromBoard(row, y));
+                insertTile(row, getRowSize(), getTileFromBoard(row + 1, y));
             }
             insertTile(x, y, tile);
         }
     }
 
     /**
-     * This method will move player when their player piece is at the end of the tile placement
-     *
-     * @param x The x co-ordinate of the new position of the player
-     * @param y The y co-ordinate of the new position of the player
-     * @param c The cardinal place of the tile insertion
+     * This method will move player when their player piece is at the end of the tile placement.
+     * @param x The x co-ordinate of the new position of the player.
+     * @param y The y co-ordinate of the new position of the player.
+     * @param c The cardinal place of the tile insertion.
      */
     public void movePlayerFromEndTile(int x, int y, Cardinals c) { //case move if end of tile, called from gameState?
         if (c == Cardinals.TOP) {
@@ -203,22 +252,22 @@ public class Board {
     }
 
     /**
-     * This method checks to see if their is a player at the end of the tile
-     *
-     * @param x The x co-ordinate of the tile on the end
-     * @param y The y co-ordinate of the tile on the end
-     * @return The Boolean value true or false
+     * This method checks to see if their is a player at the end of the tile.
+     * @param x The x co-ordinate of the tile on the end.
+     * @param y The y co-ordinate of the tile on the end.
+     * @return The Boolean value true or false.
      */
     public Boolean checkPlayerEndTile(int x, int y) {
         return getPlayerFromBoard(x, y) != null;
     }
 
+    //TODO redo using nested for loop as this is inefficient
     /**
-     *
-     * @param x
-     * @param y
-     * @param player
-     * @return
+     *This method searches and store player information.
+     * @param x The starting x co-ordinate of the search.
+     * @param y The starting y co-ordinate of the search.
+     * @param player The player in search .
+     * @return The x and y co-ordinate in as an int array.
      */
     public int[] playerLocationOnBoard(int x, int y, Player player) {
         int[] coords = new int[2];
@@ -238,12 +287,11 @@ public class Board {
     }
 
     /**
-     * This method moves player to a new position
-     *
-     * @param newX The x co-ordinate of the new position
-     * @param newY The y co-ordinate of the new position
-     * @param oldX The x co-ordinate of the old position
-     * @param oldY The x co-ordinate of the old position
+     * This method moves player to a new position.
+     * @param newX The x co-ordinate of the new position.
+     * @param newY The y co-ordinate of the new position.
+     * @param oldX The x co-ordinate of the old position.
+     * @param oldY The x co-ordinate of the old position.
      */
     public void movePlayer(int newX, int newY, int oldX, int oldY) {
         insertPlayer(newX, newY, getPlayerFromBoard(oldX, oldY));
@@ -251,22 +299,28 @@ public class Board {
     }
 
     /**
-     * This method will set the player back to their previous co-ordinate
-     *
-     * @param tilesVisited
-     * @param x
-     * @param y
+     * This method will set the player back to their previous co-ordinate.
+     * @param tilesVisited The player's previous co-ordinates history.
+     * @param x The new x co-ordinate of the player.
+     * @param y The new y co-ordinate of the player.
      */
     public void backTrackPlayer(ArrayList<Integer> tilesVisited, int x, int y) { //TODO check index of tilesVisited
-        if (getPlayerFromBoard(x, y).hasBeenDoubled) { //should be tracker on last 2 turns
-            movePlayer(tilesVisited.get(4), tilesVisited.get(5), x, y);
+        if ((getTileFromBoard(tilesVisited.get(4), tilesVisited.get(5))).onFire ||
+                (getTileFromBoard(tilesVisited.get(2), tilesVisited.get(3))).onFire) {
+            //error message here stating player cannot go back because tile is on fire
         } else {
-            movePlayer(tilesVisited.get(2), tilesVisited.get(3), x, y);
+            if (getPlayerFromBoard(x, y).hasBeenDoubled) { //should be tracker on last 2 turns
+                movePlayer(tilesVisited.get(4), tilesVisited.get(5), x, y);
+            } else {
+                movePlayer(tilesVisited.get(2), tilesVisited.get(3), x, y);
+            }
         }
+
     }
 
     /**
-     * @param tile
+     * This method will discard tiles from the board to the SilkBag.
+     * @param tile The tile being discarded.
      */
     public void discardTileToSilkBag(Tile tile) {
         SilkBag.insertTileToBag(tile);
@@ -278,5 +332,6 @@ public class Board {
         LEFT,
         RIGHT
     }
+
 
 }
