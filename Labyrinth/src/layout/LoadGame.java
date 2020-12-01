4552ca42cf6a42fa4b136528f67375bfd0cca284
Deniz <denizoral299@gmail.com>
2020-12-01 16:40:34 +0000

    Added loadgame, updated boardController.

--- Labyrinth/src/layout/LoadGame.java
+++ Labyrinth/src/layout/LoadGame.java
@@ -1,28 +1,102 @@
-//Sample code to test the connections between Menu
+package sample;
 
-package layout;
-
-import javafx.event.ActionEvent;
+import backend.Level;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.BorderPane;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import layout.Game;
+import layout.Main;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class LoadGame {
+
+public class LoadGame implements Initializable {
 
     @FXML
-    private BorderPane rootPane;
+    ListView<String> savedLevels;
 
     @FXML
-    private void initialize(){
-        //This is a check to see if window is loaded.
-        System.out.println("Loaded Load Game window.");
+    private Button backToMenu;
+    @FXML
+    private Button loadGameBtn;
+    @FXML
+    private Button deleteGameBtn;
+
+    private ArrayList<Level> level;
+
+    private Stage stage;
+
+    public LoadGame(Stage stage, ArrayList<Level> level) {
+        this.level = level;
+        this.stage = stage;
     }
 
+    int selectedIndex;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println(level.size());
+        //savedLevels.getItems().add(level.get(0));
 
-    public void goBackToMenu(ActionEvent actionEvent) throws IOException {
-        BorderPane pane = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
-        rootPane.getChildren().setAll(pane);
+        backToMenu.setOnAction(e -> {
+            goToMenu();
+        });
+
+        loadGameBtn.setOnAction(actionEvent -> {
+            loadGame();
+        });
+
+        //
+        deleteGameBtn.setOnAction(e -> {
+            deleteSaveFile();
+        });
+
+        refreshSaveList();
+    }
+
+    private void refreshSaveList() {
+        for (Level i : level) {
+            savedLevels.getItems().add(i.getBoardData().getNameOfBoard());
+        }
+    }
+
+    private void deleteSaveFile(){
+        selectedIndex = savedLevels.getSelectionModel().getSelectedIndex();
+        if (selectedIndex<0){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("ERROR");
+            alert.setHeaderText(null);
+            alert.setContentText("Please select a save file before proceeding.");
+            alert.showAndWait();
+        } else {
+            savedLevels.getItems().remove(selectedIndex);
+        }
+    }
+
+    //TODO find a better way
+    public void goToMenu() {
+        try {
+            Main main = new Main();
+            main.start(stage);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void loadGame(){
+        selectedIndex = savedLevels.getSelectionModel().getSelectedIndex();
+        if (selectedIndex<0){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("ERROR");
+            alert.setHeaderText(null);
+            alert.setContentText("Please select a save file before proceeding.");
+            alert.showAndWait();
+        } else {
+            selectedIndex = savedLevels.getSelectionModel().getSelectedIndex();
+            System.out.println(selectedIndex);
+            Game game = new Game(stage, level.get(0));
+        }
     }
 }
