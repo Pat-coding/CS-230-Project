b977d7d2ec68454c64246527210dc0fe2beb282d
Deniz <denizoral299@gmail.com>
2020-12-01 19:10:24 +0000

    Added LoadMenuLoader

---
+++ Labyrinth/src/layout/LoadMenuLoader.java
@@ -0,0 +1,30 @@
+package layout;
+
+import backend.Level;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class LoadMenuLoader {
+    public LoadMenuLoader(Stage stage, ArrayList<Level> level){
+        Stage primaryStage = stage;
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            LoadGame controller = new LoadGame(stage, level);
+            loader.setController(controller);
+            Parent root = loader.load(getClass().getClassLoader().getResource("layout/LoadGame.fxml").openStream());
+            Scene scene = new Scene(root, 600, 400);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
