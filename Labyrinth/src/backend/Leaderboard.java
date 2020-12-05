3803fcda47af9cd29e173e249e1b2d5ccbd0597d
Kane Pirankar <kanepirankar@gmail.com>
2020-12-05 23:24:59 +0000

    updated javadoc for Leaderboard

--- Labyrinth/src/backend/Leaderboard.java
+++ Labyrinth/src/backend/Leaderboard.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /**
- *This class sorts the leaderboards after a game to make sure they are up to date
+ *This class sorts the leaderboards after a game to
+ * make sure they are up to date.
  * wants them to be sorted
  * @author
  * @version 1.0.0
@@ -16,17 +17,28 @@ public class Leaderboard {
     private ArrayList<Profile> profileList;
     private int sortType;
 
+    /**
+     * This constructer determines what profiles will be sorted and how.
+     * @param profiles profiles being sorted
+     * @param sortType type of sort
+     * @param asc if it is in assending or descending order
+     */
+
     public Leaderboard(ArrayList<Profile> profiles, int sortType, boolean asc) {
         this.profileCopy = new ArrayList<>(profiles);
         this.profileList = sort(profileCopy, sortType, asc);
     }
 
+    /**
+     * Returns switch key for sorting.
+     * @return int between 0 and 3
+     */
     public int getSortType() {
         return sortType;
     }
 
     /**
-     * this method returns the leaderboard
+     * This method returns the leaderboard.
      * @return array list of profiles
      */
     public ArrayList<Profile> getLeaderboard() {
@@ -34,10 +46,10 @@ public class Leaderboard {
     }
 
     /**
-     * this method sorts a list of profiles depending on the type of sort requested
-     * @param profileList
-     * @param sortType
-     * @param asc
+     * This method sorts a list of profiles depending on the type of sort requested.
+     * @param profileList profiles getting sorted
+     * @param sortType how they're getting sorted
+     * @param asc ascending or descending order
      * @return sorted array list of profiles
      */
     public ArrayList<Profile> sort(ArrayList<Profile> profileList, int sortType, boolean asc) {
@@ -57,9 +69,9 @@ public class Leaderboard {
     }
 
     /**
-     * this methods sorts an array list of players depending on players wins
-     * @param profileList
-     * @param asc
+     * This methods sorts an array list of players depending on players wins.
+     * @param profileList profiles getting sorted
+     * @param asc ascending or descending order
      * @return array list of players in winning order
      */
     private ArrayList<Profile> sortWins(ArrayList<Profile> profileList, boolean asc) {
@@ -82,9 +94,9 @@ public class Leaderboard {
     }
 
     /**
-     * this method sorts profiles depending on number of losses they have
-     * @param profileList
-     * @param asc
+     * This method sorts profiles depending on number of losses they have.
+     * @param profileList profiles getting sorted
+     * @param asc ascending or descending order
      * @return array list of players sorted by losses
      */
 
@@ -108,9 +120,9 @@ public class Leaderboard {
     }
 
     /**
-     * this method sorts players depending on their win ration
-     * @param profileList
-     * @param asc
+     * This method sorts players depending on their win ration.
+     * @param profileList profiles getting sorted
+     * @param asc ascending or descending order
      * @return sorted array list of players based on win ratio
      */
 
@@ -134,9 +146,9 @@ public class Leaderboard {
     }
 
     /**
-     * this method sorts players by the ammount of games they have played
-     * @param profileList
-     * @param asc
+     * This method sorts players by the ammount of games they have played.
+     * @param profileList profiles getting sorted
+     * @param asc ascending or descending order
      * @return sorted array list by number of games played
      */
     private ArrayList<Profile> sortGamesPlayed(ArrayList<Profile> profileList, boolean asc) {
